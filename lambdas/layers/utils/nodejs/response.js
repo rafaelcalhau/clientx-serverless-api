10d123405908d4f3924a31d65c91cdc7a0c70d00
Rafael Calhau <calhaudev@gmail.com>
2023-09-11 18:16:52 -0300

    feat: update response util to generate specific CORS headers

--- lambdas/layers/utils/nodejs/response.js
+++ lambdas/layers/utils/nodejs/response.js
@@ -3,6 +3,9 @@ function responseHandler (status, body) {
     statusCode: status,
     body: JSON.stringify(body),
     headers: {
+      "Access-Control-Allow-Headers" : "Authorization, Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
       'Content-Type': 'application/json',
     },
   };
