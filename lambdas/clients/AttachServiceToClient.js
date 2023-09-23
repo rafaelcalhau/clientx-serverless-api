3ed975ff04adea389abc7c0895935274bdf6e58b
Rafael Calhau <calhaudev@gmail.com>
2023-09-22 21:00:23 -0300

    feat: implement lambda to attach a service to a client

--- lambdas/clients/AttachServiceToClient.js
+++ lambdas/clients/AttachServiceToClient.js
@@ -1,4 +1,7 @@
+const { ObjectId } = require('mongodb');
 const Sentry = require("@sentry/serverless");
+const { z, ZodError } = require("zod");
+const normalizeEvent = require('/opt/nodejs/normalizer');
 const response = require('/opt/nodejs/response');
 const getMongoClient = require('/opt/nodejs/getMongoClient');
 
@@ -15,11 +18,59 @@ const tracesSampleRate = typeof SENTRY_TRACES_SAMPLE_RATE === 'string'
     ? Number(SENTRY_TRACES_SAMPLE_RATE)
     : 0;
 
-const handler = async (event, context) => {
+const addServiceToClientDto = z.object({
+    serviceId: z.string(),
+});
+
+const handler = async (event) => {
     if (debugEnabled) console.log({ event });
 
-    // implement me
-    return response(200, { message: 'Not implemented.' });
+    try {
+        const mongoClient = await getMongoClient();
+        const { data, pathParameters } = normalizeEvent(event);
+        if (!pathParameters?.id) {
+            return response(400);
+        }
+
+        const doc = addServiceToClientDto.parse(data)
+        const { id } = pathParameters
+        const client = await mongoClient
+            .collection("clients")
+            .findOne({ _id: new ObjectId(id) });
+
+        if (!client) {
+            return response(404, { message: 'Client not found.' });
+        }
+
+        const { activeServices } = client
+        if (Array.isArray(activeServices)) {
+            if (activeServices.includes(doc.serviceId)) {
+                return response(200, { message: 'Client already has the service.' });
+            } else {
+                await mongoClient
+                    .collection("clients")
+                    .updateOne(
+                        { _id: new ObjectId(id) },
+                        { $set: { "activeServices.$[]": id } }
+                    )
+            }
+        } else {
+            await mongoClient
+                .collection("clients")
+                .updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: { activeServices: [id] } }
+                )
+        }
+
+        return response(200, { success: true, _id: id });
+    } catch (error) {
+        if (error instanceof ZodError) {
+            return response(500, { error, message: 'Validation error.' });
+        }
+
+        return response(500, { message: error?.message ?? error });
+    }
 };
 
 if (useSentryAPM) {
